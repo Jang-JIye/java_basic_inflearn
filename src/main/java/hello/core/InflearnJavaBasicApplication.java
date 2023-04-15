5908152873134b2a0277dc532b3b9b6a83913a84
jiyeJang <117057863+Jang-JIye@users.noreply.github.com>
2023-04-15 18:22:55 +0900

    Initial commit

---
+++ src/main/java/hello/core/InflearnJavaBasicApplication.java
@@ -0,0 +1,13 @@
+package hello.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InflearnJavaBasicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InflearnJavaBasicApplication.class, args);
+    }
+
+}
