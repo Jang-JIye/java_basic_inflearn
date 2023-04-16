37c3f943f3109f634c219345eccea36a9faf9f58
jiyeJang <117057863+Jang-JIye@users.noreply.github.com>
2023-04-17 00:55:46 +0900

    [feature] 관심사 분리 AppConfig : 실제 동작에 필요한 구현 객체 생성, 생성한 객체 인스턴스의 레퍼런스를 생성자를 통해 주입

---
+++ src/main/java/hello/core/AppConfig.java
@@ -0,0 +1,20 @@
+package hello.core;
+
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.member.MemoryMemberRepository;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+
+public class AppConfig {
+    public MemberService memberService() {
+        //실제 동작에 필요한 구현 객체 생성, 생성한 객체 인스턴스의 참조(레퍼런스)를 생성자를 통해 주입
+        return new MemberServiceImpl(new MemoryMemberRepository());
+    }
+
+    public OrderService orderService() {
+        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+    }
+
+}
