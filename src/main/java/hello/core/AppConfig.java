d18c6069e2cede7141bc7c215685ce09aae6040c
jiyeJang <117057863+Jang-JIye@users.noreply.github.com>
2023-04-21 00:28:53 +0900

    [refactor] 스프링으로 전환하기 AppConfig : @Configuration / @Bean 을 사용하여 스프링 컨테이너에 등록 -> 스프링 빈(객체) MemberApp : ApplicationContext를 사용하여 AppConfig의 빈(객체)를 찾는다. OrderApp : ApplicationContext를 사용하여 AppConfig의 빈(객체)를 찾는다.

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -8,25 +8,33 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration //'구성 정보' 라는 뜻
 public class AppConfig {
     //애플리케이션 전체 구성이 어떻게 되어있는지 빠르게 파악할 수 있다.
     //refactoring(ctrl alt m) 로 memberService / memberRepository / orderService / discountPolicy 의 역활을 전부 보여준다.
     //이렇게 하면 AppConfig 에서 바꿔주기만 하면 된다.
+    @Bean//spring container 에 등록을 해준다.
     public MemberService memberService() {
         //실제 동작에 필요한 구현 객체 생성, 생성한 객체 인스턴스의 참조(레퍼런스)를 생성자를 통해 주입
         return new MemberServiceImpl(memberRepository());
     }
 
-    private static MemoryMemberRepository memberRepository() {
+    @Bean
+    public static MemoryMemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
+        //return new FixDiscountPolicy();
         return new RateDiscountPolicy();
     }
 
